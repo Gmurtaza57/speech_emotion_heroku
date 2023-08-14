4330b6edce2caf336c835833b6a4f08d3423853b
GHULAM MURTAZA <wassan.murtaza94@gmail.com>
2023-08-14 06:22:26 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,88 @@
+from flask import Flask, request, jsonify, render_template
+import os
+import numpy as np
+import pandas as pd
+import soundfile
+import boto3
+import io
+from keras.models import load_model
+import librosa
+
+app = Flask(__name__)
+
+# Load the saved model
+loaded_model = load_model("Emotion_speech.h5")
+print("Loaded model from disk")
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/predict_emotion', methods=['POST'])
+def predict_emotion():
+    try:
+        # Get the uploaded audio file from the request
+        uploaded_file = request.files['audio']
+
+        # Print a message to confirm that the audio file was received
+        print("Received audio file:", uploaded_file.filename)
+
+        # Create a Boto3 client for S3
+        s3 = boto3.client("s3")
+
+        # Save the uploaded audio file to S3
+        bucket_name = "my-audio-bucket-1"  # Replace with your S3 bucket name
+        s3.upload_fileobj(uploaded_file, bucket_name, uploaded_file.filename)
+
+        # File URL in S3
+        file_url = f"https://{bucket_name}.s3.amazonaws.com/{uploaded_file.filename}"
+
+        # Load the audio file using librosa
+        print("Loading audio file using librosa...")
+        try:
+            X, sample_rate = librosa.load(file_url, res_type='kaiser_fast', duration=2.5, sr=22050*2, offset=0.5)
+        except Exception as e:
+            print("Error loading audio:", str(e))
+
+        print("X shape:", X.shape)
+        print("Sample rate:", sample_rate)
+        sample_rate = np.array(sample_rate)
+        mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=13), axis=0)
+        feature_live = mfccs
+        livedf2 = feature_live
+        livedf2 = pd.DataFrame(data=livedf2)
+        livedf2 = livedf2.stack().to_frame().T
+        twodim = np.expand_dims(livedf2, axis=2)
+        print("Loaded audio file using librosa...") 
+        # Make predictions
+        live_preds = loaded_model.predict(twodim, batch_size=32, verbose=1)
+        live_preds1 = live_preds.argmax(axis=1)
+        live_abc = live_preds1.astype(int).flatten()
+
+        # Define the emotion mapping
+        emotion_mapping = {
+            0: "female_angry",
+            1: "female_calm",
+            2: "female_fearful",
+            3: "female_happy",
+            4: "female_sad",
+            5: "male_angry",
+            6: "male_calm",
+            7: "male_fearful",
+            8: "male_happy",
+            9: "male_sad"
+        }
+
+        # Convert predictions to emotion labels using the provided mapping
+        live_predictions = [emotion_mapping[prediction] for prediction in live_abc]
+
+
+        # Return the prediction result in JSON format
+        return jsonify({
+            'emotion': live_predictions[0]  # Assuming you're predicting a single emotion
+        })
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/main.js
@@ -0,0 +1,92 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let recorder;
+    let recordedChunks = [];
+
+    const startButton = document.getElementById('startButton');
+    const stopButton = document.getElementById('stopButton');
+    const detectButton = document.getElementById('detectButton');
+    const listenButton = document.getElementById('listenButton');
+    const downloadRecordingButton = document.getElementById('downloadRecordingButton');
+    const downloadEmotionButton = document.getElementById('downloadEmotionButton'); // Correct ID
+
+    startButton.addEventListener('click', async () => {
+        recordedChunks = [];
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+            recorder = new MediaRecorder(stream);
+            recorder.ondataavailable = event => {
+                if (event.data.size > 0) {
+                    recordedChunks.push(event.data);
+                }
+            };
+            recorder.start();
+            startButton.disabled = true;
+            stopButton.disabled = false;
+            detectButton.disabled = true;
+        } catch (err) {
+            console.error('Error starting recording:', err);
+        }
+    });
+
+    stopButton.addEventListener('click', async () => {
+        if (recorder && recorder.state !== 'inactive') {
+            recorder.stop();
+            stopButton.disabled = true;
+            detectButton.style.display = 'block'; // Display detect button
+            detectButton.disabled = false; // Enable the detect button
+            downloadRecordingButton.style.display = 'block'; // Display download button
+            listenButton.style.display = 'block'; // Display listen button
+        }
+    });
+    
+
+    listenButton.addEventListener('click', () => {
+        const audio = document.getElementById('recordedAudio');
+        audio.play();
+    });
+
+    downloadRecordingButton.addEventListener('click', () => {
+        const blob = new Blob(recordedChunks, { type: 'audio/ogg; codecs=opus' });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.style.display = 'none';
+        a.href = url;
+        a.download = 'recorded_audio.ogg';
+        document.body.appendChild(a);
+        a.click();
+        URL.revokeObjectURL(url);
+    });
+
+    detectButton.addEventListener('click', async () => {
+        console.log('Button clicked');
+        if (recordedChunks.length === 0) {
+            console.log('No recorded audio available.');
+            return;
+        }
+    
+        const blob = new Blob(recordedChunks, { type: 'audio/wav' });
+        console.log('Blob created:', blob);
+        const formData = new FormData();
+        formData.append('audio', blob);
+        console.log('FormData created:', formData);
+        try {
+            const response = await fetch('/predict_emotion', {
+                method: 'POST',
+                body: formData
+            });
+            console.log('Fetch response:', response);
+            if (response.ok) {
+                const result = await response.json();
+                document.getElementById('result').innerText = `Predicted Emotion: ${result.emotion}`;
+                document.getElementById('result').style.display = 'block';
+                downloadEmotionButton.style.display = 'block';
+                console.log('Server message:', result.message); // Print the server message
+            } else {
+                console.error('Error predicting emotion:', response.statusText);
+            }
+        } catch (error) {
+            console.error('Error predicting emotion:', error);
+        }
+    });
+
+});
